8cad1715fb9132ee42c9e858f0c69afcac17e6aa
Dhruvsurani <dhruvsurani2000@gmail.com>
2022-09-19 18:37:53 +0530

    add some functionality

--- bank.py
+++ bank.py
@@ -1,22 +1,45 @@
+import db_connection
+
 """This is my Bank System Task"""
 
 
 class Bank:
     """This class for Creating Bank and store users details"""
+
     def __init__(self, name):
         self.bank_name = name
+
+    def check_bank(self):
+        name = self.bank_name.split()
+        for i in name:
+            if (i,) in db_conn.check_database():
+                print("'{}' Bank already exist".format(i))
+                break
+        else:
+            db_conn.create_db(bankname)
 
 
 class User:
     """This class creates new user"""
-    def __init__(self, username, pin):
+
+    def __init__(self, username, pin, bank_name):
         self.username = username
         self.pin = pin
         self.init_amount = 0
+        self.bank_name = bank_name
 
+    def check_bank(self):
+        name = self.bank_name.split()
+        for i in name:
+            if (i,) in db_conn.check_database():
+                db_conn.add_user(bank_name=i, usr_name=self.username, usr_pin=self.pin, usr_amount=self.init_amount)
+                break
+        else:
+            print("Bank not found !")
 
 class Transactions:
     """This class created for make transactions"""
+
     def __init__(self, amount, t_type, before_bal):
         self.amount = amount
         self.t_type = t_type
@@ -28,7 +51,15 @@ if __name__ == "__main__":
     while True:
         print("1) Create Account \n2) Login \n3) Delete Bank \n4) Create Bank")
         choice = int(input("Enter : "))
-
+        db_conn = db_connection.DbConnect()
         if choice == 4:
-           bankname = input("Enter Bank name : ")
-           
+            bankname = input("Enter Bank name : ")
+            bank_obj = Bank(bankname)
+            bank_obj.check_bank()
+
+        if choice == 1:
+            uname = input("\nEnter user name : ")
+            u_pin = int(input("Enter PIN : "))
+            bank_name = input("Enter Bank name : ")
+            user_obj = User(uname, u_pin, bank_name)
+            user_obj.check_bank()
--- db_connection.py
+++ db_connection.py
@@ -1,21 +1,61 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    user='postgres', password='postgres', host='localhost', port='5432'
-)
-conn.autocommit = True
 
-cursor = conn.cursor()
+class DbConnect:
+    db_name = None
 
+    def __init__(self):
+        self.db_user = 'postgres'
+        self.password = 'postgres'
+        self.db_host = 'localhost'
+        self.db_port = '5432'
+        self.db_name = ''
 
-def create_db(db_name):
-    sql = f'''CREATE database {db_name}'''
+    def db_connection(self):
+        db_connector = psycopg2.connect(user=self.db_user, password=self.password, host=self.db_host, port=self.db_port,
+                                        database=self.db_name)
+        db_connector.autocommit = True
+        cur = db_connector.cursor()
+        return cur
 
-    cursor.execute(sql)
-    print("Database created successfully........")
+    def check_database(self):
+        a = self.db_connection()
+        a.execute("SELECT datname FROM pg_database;")
+        list_database = a.fetchall()
+        return list_database
 
-    conn.close()
+    def create_db(self, bank_name):
+        a = self.db_connection()
+        self.db_name = bank_name
+        create_db = f'''CREATE database {self.db_name}'''
+        a.execute(create_db)
+        a = self.db_connection()
+        commands = (
+            '''CREATE TABLE Users(
+                user_id SERIAL PRIMARY KEY,
+                user_name VARCHAR(255) UNIQUE NOT NULL,
+                user_pin INTEGER NOT NULL,
+                user_amount INTEGER NOT NULL
+            )
+            ''',
+            '''CREATE TABLE transactions(
+                user_id SERIAL PRIMARY KEY,
+                tr_amount INTEGER NOT NULL,
+                tr_type VARCHAR(255) NOT NULL,
+                FOREIGN KEY (user_id)
+                        REFERENCES Users (user_id)
+                        ON UPDATE CASCADE ON DELETE CASCADE
+            )
+            '''
+        )
+        for command in commands:
+            a.execute(command)
 
-
-user_input = input()
-create_db(user_input)
+    def add_user(self, bank_name, usr_name, usr_pin, usr_amount):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            a.execute(f'''INSERT INTO users (user_name, user_pin, user_amount) 
+                            VALUES ('{usr_name}', {usr_pin}, {usr_amount})''')
+        except:
+            print('User already exist')
