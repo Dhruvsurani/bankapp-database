3e65243264db8635a5b149389bee144af5fd6663
Dhruvsurani <dhruvsurani2000@gmail.com>
2022-09-20 14:55:51 +0530

    final commit

--- bank.py
+++ bank.py
@@ -37,13 +37,18 @@ class User:
         else:
             print("Bank not found !")
 
-class Transactions:
-    """This class created for make transactions"""
+    def check_user(self):
+        user_in = db_conn.check_user(bank_name=self.bank_name, usr_name=self.username, usr_pin=self.pin)
+        if [(self.username, self.pin)] == user_in:
+            return True
 
-    def __init__(self, amount, t_type, before_bal):
-        self.amount = amount
-        self.t_type = t_type
-        self.before_bal = before_bal
+
+# class Transactions:
+#     """This class created for make transactions"""
+#
+#     def __init__(self, amount, t_type):
+#         self.amount = amount
+#         self.t_type = t_type
 
 
 if __name__ == "__main__":
@@ -52,10 +57,6 @@ if __name__ == "__main__":
         print("1) Create Account \n2) Login \n3) Delete Bank \n4) Create Bank")
         choice = int(input("Enter : "))
         db_conn = db_connection.DbConnect()
-        if choice == 4:
-            bankname = input("Enter Bank name : ")
-            bank_obj = Bank(bankname)
-            bank_obj.check_bank()
 
         if choice == 1:
             uname = input("\nEnter user name : ")
@@ -63,3 +64,67 @@ if __name__ == "__main__":
             bank_name = input("Enter Bank name : ")
             user_obj = User(uname, u_pin, bank_name)
             user_obj.check_bank()
+
+        elif choice == 2:
+            uname = input("\nEnter user name : ")
+            u_pin = int(input("Enter PIN : "))
+            bank_name = input("Enter Bank name : ")
+            user_obj = User(uname, u_pin, bank_name)
+            if user_obj.check_user():
+                while True:
+                    print("\n1) Deposit\n2) Withdraw \n3) Show transaction history "
+                          "\n4) Check balance \n5) Delete Account \n6) Change your PIN \n7) Logout")
+                    user_choice = int(input('Enter choice : '))
+
+                    if user_choice == 1:
+                        in_amount = int(input("Enter Deposit Amount : "))
+                        tr_type = "Deposit"
+                        db_conn.add_amount(bank_name=bank_name, usr_name=uname, usr_pin=u_pin, amount=in_amount)
+                        db_conn.save_transactions(bank_name=bank_name, usr_name=uname, usr_pin=u_pin,
+                                                  tr_amount=in_amount, tr_type=tr_type)
+
+                    elif user_choice == 2:
+                        in_amount = int(input("Enter Withdraw Amount : "))
+                        tr_type = "Withdraw"
+                        db_conn.withdraw_amount(bank_name=bank_name, usr_name=uname, usr_pin=u_pin, amount=in_amount)
+                        db_conn.save_transactions(bank_name=bank_name, usr_name=uname, usr_pin=u_pin,
+                                                  tr_amount=in_amount, tr_type=tr_type)
+
+                    elif user_choice == 3:
+                        tr_history = db_conn.tr_history(bank_name=bank_name, usr_name=uname, usr_pin=u_pin)
+                        print(tr_history)
+
+                    elif user_choice == 4:
+                        balance = db_conn.check_balance(bank_name=bank_name, usr_name=uname, usr_pin=u_pin)
+                        print(balance)
+
+                    elif user_choice == 5:
+                        db_conn.delete_account(bank_name=bank_name, usr_name=uname)
+                        break
+
+                    elif user_choice == 6:
+                        new_pin = int(input("Enter new PIN : "))
+                        reenter_pin = int(input("Re-Enter new PIN : "))
+                        if new_pin == reenter_pin:
+                            db_conn.change_pin(bank_name=bank_name, usr_name=uname, usr_pin=reenter_pin)
+                        else:
+                            print("Password don't match !")
+
+                    elif user_choice == 7:
+                        print("\n****************************")
+                        print('Thank you !')
+                        print("****************************")
+                        break
+
+                    else:
+                        print("\n****************************")
+                        print("Enter a valid Choice !")
+                        print("****************************")
+
+            else:
+                print("Wrong Username or Password !")
+
+        elif choice == 4:
+            bankname = input("Enter Bank name : ")
+            bank_obj = Bank(bankname)
+            bank_obj.check_bank()
--- db_connection.py
+++ db_connection.py
@@ -18,6 +18,8 @@ class DbConnect:
         cur = db_connector.cursor()
         return cur
 
+
+
     def check_database(self):
         a = self.db_connection()
         a.execute("SELECT datname FROM pg_database;")
@@ -27,11 +29,11 @@ class DbConnect:
     def create_db(self, bank_name):
         a = self.db_connection()
         self.db_name = bank_name
-        create_db = f'''CREATE database {self.db_name}'''
+        create_db = f'CREATE database {self.db_name}'
         a.execute(create_db)
         a = self.db_connection()
         commands = (
-            '''CREATE TABLE Users(
+            '''CREATE TABLE users(
                 user_id SERIAL PRIMARY KEY,
                 user_name VARCHAR(255) UNIQUE NOT NULL,
                 user_pin INTEGER NOT NULL,
@@ -39,11 +41,11 @@ class DbConnect:
             )
             ''',
             '''CREATE TABLE transactions(
-                user_id SERIAL PRIMARY KEY,
+                user_id INTEGER,
                 tr_amount INTEGER NOT NULL,
                 tr_type VARCHAR(255) NOT NULL,
                 FOREIGN KEY (user_id)
-                        REFERENCES Users (user_id)
+                        REFERENCES users (user_id)
                         ON UPDATE CASCADE ON DELETE CASCADE
             )
             '''
@@ -59,3 +61,93 @@ class DbConnect:
                             VALUES ('{usr_name}', {usr_pin}, {usr_amount})''')
         except:
             print('User already exist')
+
+    def check_user(self, bank_name, usr_name, usr_pin):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            data = f"SELECT user_name, user_pin FROM users WHERE user_name= '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(data)
+            return a.fetchall()
+
+        except:
+            print("user not found")
+
+    def add_amount(self, bank_name, usr_name, usr_pin, amount):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            sql = f"UPDATE users SET user_amount = user_amount + {amount} WHERE user_name = '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(sql)
+        except:
+            print("No data found !")
+
+    def withdraw_amount(self, bank_name, usr_name, usr_pin, amount):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            sql = f"UPDATE users SET user_amount = user_amount - {amount} WHERE user_name = '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(sql)
+        except:
+            print("No data found !")
+
+    def save_transactions(self, bank_name, usr_name, usr_pin, tr_amount, tr_type):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            data = f"SELECT user_id FROM users WHERE user_name= '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(data)
+            id = a.fetchall()
+            sql = f"INSERT INTO transactions (user_id, tr_amount, tr_type) VALUES ({id[0][0]}, {tr_amount},'{tr_type}')"
+            a.execute(sql)
+        except:
+            print("No data Found !")
+
+    def tr_history(self, bank_name, usr_name, usr_pin):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            data = f"SELECT user_id FROM users WHERE user_name= '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(data)
+            id = a.fetchall()
+            history = f"SELECT * FROM transactions WHERE user_id = {id[0][0]}"
+            a.execute(history)
+            return a.fetchall()
+
+        except:
+            print("No data found !")
+
+    def check_balance(self, bank_name, usr_name, usr_pin):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            data = f"SELECT user_amount FROM users WHERE user_name= '{usr_name}' AND user_pin = {usr_pin}"
+            a.execute(data)
+            return a.fetchall()[0][0]
+
+        except:
+            print("user not found")
+
+    def delete_account(self, bank_name, usr_name):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            data = f"DELETE FROM users WHERE user_name= '{usr_name}'"
+            a.execute(data)
+
+        except:
+            print("No data found !")
+
+    def change_pin(self, bank_name, usr_name, usr_pin):
+        self.db_name = bank_name
+        a = self.db_connection()
+        try:
+            sql = f"UPDATE users SET user_pin = {usr_pin} WHERE user_name = '{usr_name}'"
+            a.execute(sql)
+
+        except:
+            print("No data found !")
+
+# u = DbConnect()
+# # print(u.save_transactions('hdfc', 'dhruv', 123))
+# u.save_transactions('hdfc', 'dhruv', 123, 300)
